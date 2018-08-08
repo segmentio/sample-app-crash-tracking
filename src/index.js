08a39833bf386b021e4eca3a63a5acd7333193b7
Netto Farah <nettofarah@gmail.com>
2018-08-08 15:44:51 -0700

    Namespace globals

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ window.onerror = function (message, _source, _lineNumber, _columnNumber, error) 
     This line will give you an id in Sentry,
     so you can look up the error in Sentry later
   */
-  const sentryEventId = Raven.lastEventId();
+  const sentryEventId = window.Raven.lastEventId();
 
   /*
     This line will give you the exact point in time for when
     a crash occurred so you can watch it in FullStory
   */
-  const fullstorySessionURL = FS.getCurrentSessionURL(true);
+  const fullstorySessionURL = window.FS.getCurrentSessionURL(true);
 
   window.analytics.track('Generic JavaScript Error', {
     message: message,
