c0ae444fa19e0ecf966ee91c7511b245713cf879
Netto Farah <nettofarah@gmail.com>
2018-08-08 11:32:08 -0700

    Update readme

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 This is a sample app with examples for how to use the 
 **Understanding your website crashes with Analytics.js** Segment recipe.
+
+## Running this App
+
+1. Make sure you have `nodejs` and `yarn` installed
+2. Install dependencies:
+```bash
+$ yarn install
+```
+3. Run the App:
+```bash
+$ yarn start
+```
--- src/index.js
+++ src/index.js
@@ -7,12 +7,25 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
-
 window.onerror = function (message, _source, _lineNumber, _columnNumber, error) {
   console.error('Generic Error',  error);
 
+  /*
+    This line will give you an id in Sentry,
+    so you can look up the error in Sentry later
+  */
+  const sentryEventId = Raven.lastEventId();
+
+  /*
+    This line will give you the exact point in time for when
+    a crash occurred so you can watch it in FullStory
+  */
+  const fullstorySessionURL = FS.getCurrentSessionURL(true);
+
   window.analytics.track('Generic JavaScript Error', {
     message: message,
-    error: error.stack
+    error: error.stack,
+    sentryEventId: sentryEventId,
+    fullstorySessionURL: fullstorySessionURL
   });
 }
